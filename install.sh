741aa963599d0a6e21e81d2e1def59227c0727b4
Sam <samuel.johnson.bungie+github@gmail.com>
2021-02-23 21:09:12 -0500

    Update install.sh
    change pathing from ~ to .. so that dotfiles can be installed in /etc/skel and copied down to newly created users properly.

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 # Find all dot files then if the original file exists, create a backup
 # Once backed up to {file}.dtbak symlink the new dotfile in place
 for file in $(find . -maxdepth 1 -name ".*" -type f  -printf "%f\n" ); do
-    if [ -e ~/$file ]; then
-        mv -f ~/$file{,.dtbak}
+    if [ -e ../$file ]; then
+        mv -f ../$file{,.dtbak}
     fi
-    ln -s $PWD/$file ~/$file
+    ln -s $PWD/$file ../$file
 done
 
 echo "Installed"
